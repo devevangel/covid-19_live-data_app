4b0eedf89ef55d05d3c9c45368f0d9a6ee63b0d8
Evangel <40840123+mick7694@users.noreply.github.com>
2020-04-26 22:14:52 +0100

    Updates

---
+++ precache-manifest.f54300740f9297b97bb06475beb0ecf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061dbdbf6a1ba5a1f14beb4cff53301f",
+    "url": "/covid-19_live-data_app/index.html"
+  },
+  {
+    "revision": "5b07486ec6bead119494",
+    "url": "/covid-19_live-data_app/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "1a281f453acaf5addaed",
+    "url": "/covid-19_live-data_app/static/css/main.95c8131b.chunk.css"
+  },
+  {
+    "revision": "5b07486ec6bead119494",
+    "url": "/covid-19_live-data_app/static/js/2.421dbc14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19_live-data_app/static/js/2.421dbc14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a281f453acaf5addaed",
+    "url": "/covid-19_live-data_app/static/js/main.7b5eb811.chunk.js"
+  },
+  {
+    "revision": "25017da9465e5e868a4a",
+    "url": "/covid-19_live-data_app/static/js/runtime-main.d75aa5a4.js"
+  }
+]);
